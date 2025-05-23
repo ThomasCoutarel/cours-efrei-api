6872a00b6fcb3296cf80850d31a492c7174ba57b
Thomas Coutarel <thomascout@hotmail.com>
2025-05-23 15:18:08 +0200

    essai bon  pow v2

--- pow.test.js
+++ pow.test.js
@@ -2,6 +2,6 @@ import {Pow} from "./pow";
 
 describe ('testing pow function', () => {
     test('should return 4', () => {
-        expect(Pow(2,2)).toBe(4);
+        expect(Pow(2,2)).toBe(5);
     });
 })
